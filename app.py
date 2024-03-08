002d3b8701e64450c4739a4f9f360f4a0ddb82b1
Mark Moralls <mmoralls0@gmail.com>
2024-03-08 17:05:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 # import packages
 
 from data import extract_data
-import smtplib, os, ssl
+import smtplib
+import ssl
+import os
 from dotenv import load_dotenv
 _ = load_dotenv()
 
 extract_data()
 
-def send_mail():
-    email_address = os.environ.get("gmail_username")
-    email_password = os.environ.get("gmail_password")
+email_address = os.environ.get("gmail_username")
+email_password = os.environ.get("gmail_password")
 
-    port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
-    sender_email = email_address
-    # Enter receiver address
-    receiver_email = "mmoralls0@gmail.com"
-    password = email_password
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = email_address
+password = email_password
+receiver_email = "mmoralls0@gmail.com", "jeraldinemoralls@gmail.com"
 
-    #finally!; file is opened, read and parsed into the email
-    with open("weather.txt", "r") as my_file:
-        file_content = my_file.read()
 
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, file_content)
-        server.quit()
+with open("weather.txt", "r") as my_file:
+    file_content = my_file.read()
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, file_content)
+    server.quit()
