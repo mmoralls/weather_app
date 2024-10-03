83a110c017aa8666aa063c9e156af1cfd3db9136
Mark Moralls <mmoralls0@gmail.com>
2024-10-03 17:05:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from api_v3 import extract_data
 from email_with_msg import send_mail
+import streamlit as st
+
+st.write('This app uses an api connected to weather.gov and extracts local weather data in my city.')   
+st.write ('Check email for response.')
+    
+with st.container():
+
+    col1 = st.columns([6])
 
 extract_data()
 
